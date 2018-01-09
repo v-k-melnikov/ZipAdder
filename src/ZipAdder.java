352e25b302de09dd5936d31b7bd996f1201c7c5c
Vlad Melnikov <v.k.melnikov.92@gmail.com>
2018-01-09 14:55:32 +0300

    1

--- src/ZipAdder.java
+++ src/ZipAdder.java
@@ -17,7 +17,8 @@ import java.util.zip.ZipOutputStream;
  * Then if this file exists in a archive, ZipAdder replaces content of this file by content
  * of our file. If file doesn't exists, ZipAdder creates a directory 'new' and puts file there.
  **/
-
+//1
+    //2
 public class ZipAdder {
 
     public static void main(String[] args) throws IOException {
